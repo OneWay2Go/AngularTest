e63c59267a828adbe6c820abe3babcee3f642ad6
OneWay2Go <uchqunov.m@yandex.com>
2025-06-12 18:45:57 +0500

    Angular Playground

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { FirstPage } from './pages/first-page/first-page';
+import { SecondPage } from './pages/second-page/second-page';
+import { GetUsers } from './pages/get-users/get-users';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'first-page', component: FirstPage},
+    {path: 'second-page', component: SecondPage},
+    {path: 'get-users', component: GetUsers}
+];
---
+++ src/app/pages/first-page/first-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-first-page',
+  imports: [],
+  templateUrl: './first-page.html',
+  styleUrl: './first-page.css'
+})
+export class FirstPage {
+
+}
