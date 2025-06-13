ac68da986e7cc75898a26d776cd69c337eda23cc
OneWay2Go <uchqunov.m@yandex.com>
2025-06-13 11:43:22 +0500

    Get Students Api Added

--- src/app/pages/get-users/get-users.ts
+++ src/app/pages/get-users/get-users.ts
@@ -1,11 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-get-users',
-  imports: [],
+  imports: [HttpClient, Observable],
   templateUrl: './get-users.html',
   styleUrl: './get-users.css'
 })
-export class GetUsers {
 
+class Student 
+{
+  public Id!: number;
+  public FirstName!: string;
+  public LastName!: string;
+  public Age!: number;
+  public Email!: string;
+}
+
+export class GetUsers {
+  users: Student[] = [];
+
+  constructor(private http: HttpClient) {  }
+
+  getAllStudents(){
+    this.http.get<Student[]>('https://localhost:7136/api/Students')
+      .subscribe(data => {
+        this.users = data;
+      });
+  }
 }
