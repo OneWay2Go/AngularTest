e03f7a39989d0c0a3e36e135d6f7a7059676ca98
OneWay2Go <uchqunov.m@yandex.com>
2025-06-13 13:38:08 +0500

    Some change

--- src/app/pages/get-by-id-contact/get-by-id-contact.ts
+++ src/app/pages/get-by-id-contact/get-by-id-contact.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-get-by-id-contact',
@@ -13,26 +14,24 @@ export class GetByIdContact {
 
   public contact:any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {  }
 
-  getContactById() {
+  getContactById(){
     if (this.Id === undefined || this.Id === null) {
       alert('Please enter a valid contact ID.');
-      return;
     }
     else{
       this.http.get(`http://45.92.173.46:5050/api/Contacts/GetByIdContact?contactID=${this.Id}`)
-      .subscribe({
-        next: (data) => {
+      .subscribe((data) => {
           if (data === null || data === undefined) {
             alert('No contact found with the provided ID.');
             this.contact = null;
           }
           else {
             this.contact = data;
+            console.log(this.contact);
           }
-          console.log(this.contact);
-        }});
+        });
     }
   }
 }
